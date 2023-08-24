b0f6205666def48cafd913f0da8209a4c6d9086c
Gilles AHOUANTCHEDE <gilles@cyberspector.com>
2023-08-24 09:29:03 +0100

    feat: first commit

---
+++ app/(homes)/home-4/page.jsx
@@ -0,0 +1,57 @@
+
+
+
+
+
+import React from 'react'
+import HeaderFour from '@/components/layout/headers/HeaderFour'
+import HeroFour from '@/components/homes/heros/HeroFour'
+import Brands from '@/components/common/Brands'
+
+import CategoriesFour from '@/components/homes/categories/CategoriesFour'
+import CoursesFour from '@/components/homes/courses/CoursesFour'
+import LearningPath from '@/components/homes/LearningPath/LearningPath'
+import FeaturedCourses from '@/components/homes/courses/FeaturedCourses'
+import LandJob from '@/components/homes/landJob/LandJob'
+import TestimonialsFour from '@/components/homes/testimonials/TestimonialsFour'
+import AchievementsTwo from '@/components/homes/achievements/AchievementsTwo'
+import InstractorFour from '@/components/homes/instractors/InstractorFour'
+import EventsFour from '@/components/homes/events/EventsFour'
+import LearningSelection from '@/components/homes/LearningSelection'
+import FooterFour from '@/components/layout/footers/FooterFour'
+import Preloader from '@/components/common/Preloader'
+export const metadata = {
+  title: 'laabdaa',
+  description:
+    'Elevate your e-learning content with Laabdaa, the most impressive platforms for online courses and education.',
+}
+
+
+
+
+export default function page() {
+  return (
+    <>
+    <div className='main-content'>
+      <Preloader/>
+        <HeaderFour/>
+      </div>
+      <div className="content-wrapper  js-content-wrapper overflow-hidden">
+        <HeroFour/>
+        <Brands/>
+        <CategoriesFour/>
+        <CoursesFour/>
+        <LearningPath/>
+        <FeaturedCourses/>
+        <LandJob/>
+        <TestimonialsFour/>
+        <AchievementsTwo/>
+        <InstractorFour/>
+        <EventsFour/>
+        <LearningSelection/>
+        <FooterFour/>
+
+     </div>
+     </>
+  )
+}

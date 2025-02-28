49cab38ca0bf41db88a5182bc67121627b06a226
rootgil <gillesahouantchede@gmail.com>
2025-02-28 11:41:54 +0100

    Refactor Video component to use Plyr for enhanced video playback and subtitle support

--- components/aboutCourses/lesson/Video.jsx
+++ components/aboutCourses/lesson/Video.jsx
@@ -1,63 +1,44 @@
 "use client";
 
 import React, { useEffect, useState, useRef } from "react";
-import ModalVideoComponent from "../../common/ModalVideo";
-import Image from "next/image";
+import Plyr from "plyr-react";
+import "plyr-react/plyr.css";
 
-export default function Video({url, sub}) {
-
-  // TODO: have id for index
-  // TODO: Map and switch audio track
-
-  const [isOpen, setIsOpen] = useState(false);
-
-  let recognition = null
-
-  const handleVideoEnd = () => {
-    alert('La vidéo est terminée.');
-
-    // TODO: send finished to API
-  };
-
-  const videoRef = useRef(null);
+export default function Video({ url, sub }) {
+  const playerRef = useRef(null);
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    // Mise à jour de la source de la vidéo lorsque l'URL change
-    if (videoRef.current) {
-      videoRef.current.load();
-    }
-  }, [url]);
+    setIsMounted(true);
+  }, []);
+
+
+  const sources = {
+    type: "video",
+    sources: [{ src: url, type: "video/mp4" }],
+    tracks: sub
+      ? sub.map((el_sub, id) => ({
+          kind: "subtitles",
+          src: `/api/proxy?url=${encodeURIComponent(el_sub.link)}`,
+          srclang: el_sub.lang,
+          label: el_sub.lang,
+          default: id === 0,
+        }))
+      : [],
+  };
 
   return (
-    <>
-      <div className="">
-        <video 
-          ref={videoRef}
-          controls
-          style={{width: '100%', height: "auto"}}
-          onEnded={handleVideoEnd}
-        >
-          <source 
-            src={url} 
-            type="video/mp4" 
-            style={{width: '100% !important'}}
-          />
-          
-          {sub && sub.map((el_sub, id) => <track
-              label={el_sub.lang}
-              kind="subtitles"
-              srcLang={el_sub.lang}
-              src={`/api/proxy?url=${encodeURIComponent(el_sub.link)}`}
-              default={id==0}
-              key={id}
-          />)}
-        </video>
-      </div>
-      {/* <ModalVideoComponent
-        videoId={"LlCwHnp3kL4"}
-        isOpen={isOpen}
-        setIsOpen={setIsOpen}
-      /> */}
-    </>
+    <div className="video-container">
+      {isMounted && (
+        <Plyr
+          ref={playerRef}
+          source={sources}
+          options={{
+            controls: ["play", "progress", "current-time", "mute", "volume", "settings"],
+            settings: ["captions", "quality", "speed"],
+          }}
+        />
+      )}
+    </div>
   );
 }
